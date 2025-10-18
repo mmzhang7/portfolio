a5993b84c52d61d29add4573d6e8f37d705ec2ba
mmzhang7 <167454772+mmzhang7@users.noreply.github.com>
2025-10-17 21:03:20 -0700

    Update global.js

--- global.js
+++ global.js
@@ -11,18 +11,27 @@ let pages = [
   { url: "resume/", title: "Resume" },
   { url: "https://github.com/mmzhang7", title: "Profile" },
 ];
-
-const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/" 
-    : "/portfolio/"; 
     
 let nav = document.createElement("nav");
 document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
 
 for (let p of pages) {
   let url = p.url;
   url = !url.startsWith('http') ? BASE_PATH + url : url;
   let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  if (a.host === location.host && a.pathname === location.pathname) {
+  a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+   
 }

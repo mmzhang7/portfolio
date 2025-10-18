cfaa314ae2d44eaab233fe36e6e536c08146280f
mmzhang7 <167454772+mmzhang7@users.noreply.github.com>
2025-10-17 20:15:12 -0700

    changing nav into js

--- global.js
+++ global.js
@@ -3,3 +3,27 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/mmzhang7", title: "Profile" },
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/" 
+    : "/portfolio/"; 
+
+let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
+    
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
